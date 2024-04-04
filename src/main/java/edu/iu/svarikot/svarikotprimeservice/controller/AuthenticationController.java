492681d640ec17d28a3be2031371323253c0fccc
SAI SATHWIK REDDY VARIKOTI <varikotisaisathwikreddy@gmail.com>
2024-04-04 19:43:51 -0400

    rabbitMq

--- src/main/java/edu/iu/svarikot/svarikotprimeservice/controller/AuthenticationController.java
+++ src/main/java/edu/iu/svarikot/svarikotprimeservice/controller/AuthenticationController.java
@@ -6,6 +6,7 @@ import edu.iu.svarikot.svarikotprimeservice.service.TokenService;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 
 @RestController
+@CrossOrigin({"http://127.0.0.1:5500"})
 public class AuthenticationController {
     private final IAuthenticationService authenticationService;
     private final AuthenticationManager authenticationManager;
--- src/main/java/edu/iu/svarikot/svarikotprimeservice/controller/PrimesController.java
+++ src/main/java/edu/iu/svarikot/svarikotprimeservice/controller/PrimesController.java
@@ -1,6 +1,7 @@
 package edu.iu.svarikot.svarikotprimeservice.controller;
 
 
+import edu.iu.svarikot.svarikotprimeservice.rabbitmq.MQSender;
 import edu.iu.svarikot.svarikotprimeservice.service.IPrimesService;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,11 +10,15 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/primes")
 public class PrimesController {
     IPrimesService primesService;
-    public PrimesController(IPrimesService primesService){
+    private final MQSender mqSender;
+    public PrimesController(IPrimesService primesService, MQSender mqSender){
         this.primesService = primesService;
+        this.mqSender = mqSender;
     }
     @GetMapping("/{n}")
     public boolean isPrime(@PathVariable("n") int n) {
-        return primesService.isPrime(n);
+        boolean result = primesService.isPrime(n);
+        mqSender.sendMessage(n, result);
+        return result;
     }
 }
